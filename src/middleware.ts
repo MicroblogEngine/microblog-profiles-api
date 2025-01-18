fad1fe8137f830a8d8be4fe0225c49911b915e00
Rogerio Pereira Araujo <rogerio.araujo@gmail.com>
2025-01-18 09:41:36 -0300

    Bypass unit tests and other fixes.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,39 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 import { jwtVerify } from 'jose'
 
+/*
 const ignoredRoutes = [
   '/api/users/health',
   '/api/users/metrics',
 ]
-
-const allowedOrigins = [
-  'https://appmicroblogararog.loclx.io', 
-  'https://app.microblog.training', 
-  'http://microblog-prometheus.default.svc.cluster.local', 
-];
+*/
 
 export async function middleware(req:NextRequest) {
   const res = NextResponse.next();
 
-  const origin = req.headers.get("origin");
-  if (! origin)
-    return res;
-
-  // if the origin is an allowed one,
-  // add it to the 'Access-Control-Allow-Origin' header
-  if (allowedOrigins.includes(origin)) {
-    res.headers.append('Access-Control-Allow-Origin', origin);
-  }
-
-  // add the remaining CORS headers to the response
-  res.headers.append('Access-Control-Allow-Credentials', "true")
-  res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-  res.headers.append(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-
-  if(! ignoredRoutes.includes(req.url)) {
+  if(! req.url.includes('/health') && 
+    ! req.url.includes('/metrics') && 
+    ! req.url.includes('/auth')) {
     const authorization = req.headers.get('authorization');
     if(!authorization)
       return new NextResponse(null, { status: 401 });
