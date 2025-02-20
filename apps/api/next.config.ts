6eb621889fc05bdb05491f9bd5e6812450bb17ae
Rogerio Pereira Araujo <rogerio.araujo@gmail.com>
2025-02-20 14:12:11 -0300

    [chore] adding dockerignore

--- apps/api/next.config.ts
+++ apps/api/next.config.ts
@@ -3,6 +3,27 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  headers: async () => {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { 
+            key: "Access-Control-Allow-Origin", 
+            value: "*" 
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },  
   webpack: (config, {}) => {
     config.resolve.alias['@'] = path.join(__dirname, 'src', 'app');
     return config;
